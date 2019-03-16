18ef5f5c566f11e7bcadc119e3c7841fc5abc90a
naoki hirata <naoki@magic3.org>
2019-03-16 19:34:37 +0900

    テスト用スクリプト更新。

--- test/build_full_magic3.sh
+++ test/build_full_magic3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/magic3org/oneliner-env/master/script/build_full_magic3.sh | sh /dev/stdin -test
+curl https://raw.githubusercontent.com/magic3org/oneliner-env/master/script/build_full_magic3.sh | bash /dev/stdin -test
